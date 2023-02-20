c286581b345b28f18880c8e7c69aaba79331591e
Divyansh Nishad <90184714+divyansh-nishad@users.noreply.github.com>
2023-02-20 22:33:47 +0530

    -onboarding screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:twentyfour/screens/onboard_screen/on_boarding_screen.dart';
+import 'package:twentyfour/screens/login/onboarding.dart';
+// import 'package:twentyfour/screens/onboard_screen/on_boarding_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const OnBoardingScreen(),
+      home: HomeScreen(),
     );
   }
 }
